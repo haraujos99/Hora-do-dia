e07569ac2fe9de6018c9282ba09cf66f4f2cfc21
Henrique Araujo <91489511+haraujos99@users.noreply.github.com>
2022-03-16 17:07:39 -0300

    Deixei parecendo um código de verdade agora

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function carregar(){
 var msg = window.document.getElementById('msg');
 var img = window.document.getElementById('imagem');
 var data = new Date();
-var hora = data.getHours();
-var minutos = data.getMinutes();
-hora = 20
+var hora = Number(data.getHours());
+var minutos = Number(data.getMinutes());
+
 if(minutos < 10){
     
     msg.innerHTML = `${hora}:0${minutos}`
@@ -16,18 +16,15 @@ if(minutos < 10){
 if(hora >= 0 && hora < 12){
   
    img.src = 'Manhã.jpg';
-   //img.innerHTML = '<img src="Manhã.jpg">'
     document.body.style.background = '#e2cd9f'
     
 } else if (hora >= 12 && hora < 18) {
 
     img.src = 'Tarde.jpg';
-    //img.innerHTML = '<img src="Tarde.jpg">'
     document.body.style.background = '#b9846f'
 } else {
 
     img.src = 'noite.jpg';
-    //img.innerHTML = '<img src="noite.jpg">'
     document.body.style.background = '#515154'
 }
 }
